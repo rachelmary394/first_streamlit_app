0937527988bad4a3c88c8f1dcd01c4cc14f385ea
Rachel Andrews <105467816+rachelmary394@users.noreply.github.com>
2022-05-12 18:10:17 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
 streamlit.title('My parents new healthy diner')
-streamlit.print('Breakfast')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 and Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
