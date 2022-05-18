ad1be05d17c6814f6822ed1caf88fde078473d9f
Rachel Andrews <105467816+rachelmary394@users.noreply.github.com>
2022-05-17 17:44:54 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-from urllip.error import URLError
+from urllib.error import URLError
 streamlit.title('My parents new healthy diner')
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
